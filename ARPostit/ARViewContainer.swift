974f2e31c3ded9d9422a7fb7238b5a64fb20bda8
bsorrentino <bartolomeo.sorrentino@gmail.com>
2023-07-27 11:44:24 +0200

    feat: add plane detection and drawing

--- ARPostit/ARViewContainer.swift
+++ ARPostit/ARViewContainer.swift
@@ -46,21 +46,26 @@ struct ARViewContainer: View {
 struct ARViewContainerRepresentable: UIViewRepresentable {
     @Binding var notes: [String: [CGPoint]]
 
-    let arView = ARView(frame: .zero)
-    let configuration = ARWorldTrackingConfiguration()
+//    let arView = ARView(frame: .zero)
+    let arView = ARView()
+    
     
     
     func makeUIView(context: Context) -> ARView {
+        // [RealityKit tutorial: Plane Detection and Raycasting](https://www.youtube.com/watch?v=T1u1tyMlMLM&t=101s)
+        arView.automaticallyConfigureSession = true
+        arView.debugOptions = [ .showSceneUnderstanding, .showFeaturePoints ]
         
+        let configuration = ARWorldTrackingConfiguration()
         configuration.planeDetection = [.vertical]
+        configuration.environmentTexturing = .automatic
+        
         arView.session.run(configuration)
 
         let tapGesture = UITapGestureRecognizer(target: context.coordinator,
-                                                action: #selector(context.coordinator.addNoteToWall(_:)))
-        /*
-        let tapGesture = UITapGestureRecognizer(target: context.coordinator,
-                                                action: #selector(context.coordinator.handleTap(_:)))
-         */
+                                                action: #selector(context.coordinator.handleTap_and_raycast(_:)))
+//                                                action: #selector(context.coordinator.handleTap_and_hitTest(_:)))
+         
         arView.addGestureRecognizer(tapGesture)
         return arView
     }
@@ -71,46 +76,114 @@ struct ARViewContainerRepresentable: UIViewRepresentable {
         Coordinator(self)
     }
     
-    class Coordinator: NSObject {
+      // FROM BING: prompt[which are events related to detected planes]
+//    func planesDetectionSetup() {
+//        
+//        let session = ARKitSession()
+//        let planeData = PlaneDetectionProvider(alignments: [.horizontal, .vertical])
+//
+//        Task {
+//            try await session.run([planeData])
+//            
+//            for await update in planeData.anchorUpdates {
+//                // Skip planes that are windows.
+//                if update.anchor.classification == .window { continue }
+//                
+//                switch update.event {
+//                case .added, .updated:
+//                    updatePlane(update.anchor)
+//                case .removed:
+//                    removePlane(update.anchor)
+//                }
+//            }
+//        }
+//
+//    }
+    class Coordinator: NSObject, ARSessionDelegate {
         var parent: ARViewContainerRepresentable
+        var grids = [Grid]()
         
         init(_ parent: ARViewContainerRepresentable) {
             self.parent = parent
+            super.init()
+            self.parent.arView.session.delegate = self
         }
-        
-// FROM PHIND
-//
-//        @objc func handleTap(_ sender: UITapGestureRecognizer) {
-//            let location = sender.location(in: parent.arView)
-//            let results = parent.arView.raycast(from: location,
-//                                                     allowing: .estimatedPlane,
-//                                                     alignment: .vertical)
-//
-//            if let firstResult = results.first {
-//                let anchor = ARAnchor(name: "text", transform: firstResult.worldTransform)
-//                parent.arView.session.add(anchor: anchor)
-//            }
-//        }
-
-        @objc func addNoteToWall(_ sender: UITapGestureRecognizer) {
-            let arView = parent.arView
-            let tapLocation = sender.location(in: arView)
-            if let hitTestResult = arView.hitTest(tapLocation, types: .existingPlane).first {
-                let position = SCNVector3(hitTestResult.worldTransform.columns.3.x,
-                                         hitTestResult.worldTransform.columns.3.y,
-                                         hitTestResult.worldTransform.columns.3.z)
-                
-                let anchor = ARAnchor(name: "Note", transform: hitTestResult.worldTransform)
-                arView.session.add(anchor: anchor)
-                
-                let noteText = "New Note"
-                if var existingNotes = parent.notes[noteText] {
-                    existingNotes.append(CGPoint(x: CGFloat(position.x), y: CGFloat(position.y)))
-                    parent.notes[noteText] = existingNotes
-                } else {
-                    parent.notes[noteText] = [CGPoint(x: CGFloat(position.x), y: CGFloat(position.y))]
+     
+        // FROM BING: prompt[Can you show me how to detect vertical planes?]
+        func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+            for anchor in anchors {
+                if let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .vertical {
+                    // Add a new Grid entity to your scene for the detected vertical plane
+                    let grid = Grid(planeAnchor: planeAnchor)
+                    parent.arView.scene.addAnchor(grid)
+                    grids.append(grid)
                 }
             }
+        }
+        
+        // [RealityKit – Visualizing Grid on detected planes](https://stackoverflow.com/a/71351712/521197)
+        func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+                
+                guard let planeAnchor = anchors[0] as? ARPlaneAnchor else { return }
+            
+            if let updatedGrid = grids.first( where: { $0.planeAnchor.identifier == planeAnchor.identifier } ) {
+                updatedGrid.didUpdate(anchor: planeAnchor)
+            }
+//                let grid: Grid? = grids.filter { grd in
+//                    grd.planeAnchor.identifier == planeAnchor.identifier }[0]
+//                guard let updatedGrid: Grid = grid else { return }
+            
+            }
+        //
+        // FROM PHIND:
+        //
+        @objc func handleTap_and_raycast(_ sender: UITapGestureRecognizer) {
+            let location = sender.location(in: parent.arView)
+            
+            let results = parent.arView.raycast(from: location,
+                                                     allowing: .estimatedPlane,
+                                                     alignment: .vertical)
+
+            if results.isEmpty {
+                print("no plane detect at \(location)!")
+            }
+            else {
+                addNoteToWall( worldTransform: results.first!.worldTransform)
+            }
+
+        }
+
+        @objc func handleTap_and_hitTest(_ sender: UITapGestureRecognizer) {
+            
+            let location = sender.location(in: parent.arView)
+            
+            let results = parent.arView.hitTest(location, types: .existingPlane)
+            
+            if results.isEmpty {
+                print("no plane detect at \(location)!")
+            }
+            else {
+                addNoteToWall( worldTransform: results.first!.worldTransform)
+            }
+        }
+        
+        func addNoteToWall( worldTransform: simd_float4x4 ) {
+            
+            let position = SCNVector3(worldTransform.columns.3.x,
+                                     worldTransform.columns.3.y,
+                                     worldTransform.columns.3.z)
+            
+            let anchor = ARAnchor(name: "Note", transform: worldTransform)
+            parent.arView.session.add(anchor: anchor)
+            
+            let noteText = "New Note"
+            if var existingNotes = parent.notes[noteText] {
+                existingNotes.append(CGPoint(x: CGFloat(position.x), y: CGFloat(position.y)))
+                parent.notes[noteText] = existingNotes
+            } else {
+                parent.notes[noteText] = [CGPoint(x: CGFloat(position.x), y: CGFloat(position.y))]
+            }
+
         }
     }
 }
---
+++ ARPostit/Grid.swift
@@ -0,0 +1,56 @@
+//
+//  Grid.swift
+//  ARPostit
+//
+//  Created by Bartolomeo Sorrentino on 27/07/23.
+//
+import RealityKit
+import ARKit
+
+class Grid: Entity, HasModel, HasAnchoring {
+    var planeAnchor: ARPlaneAnchor
+    var planeGeometry: MeshResource!
+    
+    init(planeAnchor: ARPlaneAnchor) {
+        self.planeAnchor = planeAnchor
+        super.init()
+        self.didSetup()
+    }
+    
+    fileprivate func didSetup() {
+        
+        
+        do {
+            let texture  = MaterialParameters.Texture( try .load(named: "grid") )
+
+            self.planeGeometry = .generatePlane(width: planeAnchor.planeExtent.width, depth: planeAnchor.planeExtent.height)
+            var material = UnlitMaterial()
+            material.color = .init(tint: .white.withAlphaComponent(0.999), texture: texture )
+            let model = ModelEntity(mesh: planeGeometry, materials: [material])
+            model.position = [planeAnchor.center.x, 0, planeAnchor.center.z]
+            self.addChild(model)
+
+            self.transform.matrix = planeAnchor.transform
+        }
+        catch {
+            print( "GRID SETUP ERROR: \(error)")
+        }
+        
+        
+    }
+    
+    func didUpdate(anchor: ARPlaneAnchor) {
+        
+        self.transform.matrix = planeAnchor.transform
+        
+        self.planeGeometry = .generatePlane(width:  planeAnchor.planeExtent.width, depth: planeAnchor.planeExtent.height)
+        let pose: SIMD3<Float> = [anchor.center.x, 0, anchor.center.z]
+        let model = self.children[0] as! ModelEntity
+        model.position = pose
+    }
+    
+    required init() {
+        fatalError("Hasn't been implemented yet")
+    }
+}
+
